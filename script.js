3b122a438df64c8d8f8eb726d600103f9d2b9eb1
michkubik <michal.kubik@o2.pl>
2021-02-21 02:47:07 +0100

    Initial commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-   const apiRoot = 'https://tranquil-earth-77320.herokuapp.com/v1/task/getTasks';
+   const apiRoot = 'https://tranquil-earth-77320.herokuapp.com/v1/task/';
    const trelloApiRoot = 'https://tranquil-earth-77320.herokuapp.com/v1/trello/';
    const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
    const $tasksContainer = $('[data-tasks-container]');
